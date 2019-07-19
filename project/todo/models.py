3406ec0f9c28823c4b2e7760925f185fcbfe6b28
tatuu <tatuutatuu0906@gmail.com>
2019-07-19 12:22:29 +0900

    [add]base.htmlの追加
    webページのヘッダー部分をbase.htmlに
    これによりindex.htmlの内容を変更（機能、見た目に変更はなし）

--- project/todo/models.py
+++ project/todo/models.py
@@ -1,10 +1,10 @@
+import uuid
 from django.db import models
 from django.utils import timezone
 
 class List(models.Model):
     title = models.CharField(max_length=200)
     create_date = models.DateTimeField(default=timezone.now)
-    
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
@@ -12,5 +12,6 @@ class Task(models.Model):
     create_date = models.DateTimeField(default=timezone.now)
     deadline_date = models.DateTimeField(blank=True, null=True)
     completed = models.BooleanField(default=False)
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
 
 # Create your models here.
